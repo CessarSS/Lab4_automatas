dfdb4735b8875a51be1eed13adab76a0cf3df642
CessarSS <124132714+CessarSS@users.noreply.github.com>
2023-12-13 10:01:06 -0500

    analizador.js

--- analizador.js
+++ analizador.js
@@ -1,58 +1,54 @@
-  // Definición de tokens y expresiones regulares
-  const tokenRegex = /^(cuara|peso|dólar)$/;
-  const amountRegex = /^(0\.25|0\.50|1\.00)$/;
+// Definición de tokens y expresiones regulares
+const tokenRegex = /^(cuara|peso|dólar)$/i;  // La 'i' hace que sea insensible a mayúsculas/minúsculas
+const amountRegex = /^(0\.25|0\.50|1\.00)$/;
 
+// Función para el análisis léxico
 function lexicalAnalysis(input) {
-    // ...
-    const tokens = input.trim().split(/\s+/);
-    const lexemes = [];
-    for (const token of tokens) {
-        if (token.match(tokenRegex)) {
-        lexemes.push(token);
-        } else if (token.match(amountRegex)) {
-        // Convertir el valor numérico
-        const value = parseFloat(token);
-        lexemes.push(value);
-        } else {
-        // Token no válido
-        console.error(`Token no válido: ${token}`);
-        return null;
-        }
-    }
-    return lexemes;
-  }
-  
-  function syntacticAnalysis(lexemes) {
-    // ...
-    let totalAmount = 0;
+  const tokens = input.trim().split(/\s+/);
+  const lexemes = [];
 
-    for (const lexeme of lexemes) {
-        if (typeof lexeme === 'number') {
-        totalAmount += lexeme;
-        }
-    }
-    return totalAmount;
-  }
-  
-  function analizar() {
-    const userInput = document.getElementById('userInput').value;
-    const lexemes = lexicalAnalysis(userInput);
-  
-    if (lexemes) {
-      const totalAmount = syntacticAnalysis(lexemes);
-  
-      if (totalAmount !== null) {
-        const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = `
-          <p>Análisis léxico exitoso. Saldo total: ${totalAmount.toFixed(2)}</p>
-          <p>${totalAmount >= 1.00 ? '¡Saldo suficiente para realizar una compra!' : 'Saldo insuficiente para realizar una compra.'}</p>
-        `;
-      }
+  for (const token of tokens) {
+    if (token.match(tokenRegex)) {
+      lexemes.push({ type: 'TOKEN', value: token });
+    } else if (token.match(amountRegex)) {
+      const value = parseFloat(token);
+      lexemes.push({ type: 'AMOUNT', value });
+    } else {
+      console.error(`Token no válido: ${token}`);
+      return null;
     }
   }
 
-  
+  return lexemes;
+}
 
+// Función para el análisis sintáctico
+function syntacticAnalysis(lexemes) {
+  let totalAmount = 0;
 
+  for (const lexeme of lexemes) {
+    if (lexeme.type === 'AMOUNT') {
+      totalAmount += lexeme.value;
+    }
+  }
 
- 
+  return totalAmount;
+}
+
+// Función para analizar
+function analizar() {
+  const userInput = document.getElementById('userInput').value;
+  const lexemes = lexicalAnalysis(userInput);
+
+  if (lexemes) {
+    const totalAmount = syntacticAnalysis(lexemes);
+
+    if (totalAmount !== null) {
+      const resultDiv = document.getElementById('result');
+      resultDiv.innerHTML = `
+        <p>Análisis léxico exitoso. Saldo total: ${totalAmount.toFixed(2)}</p>
+        <p>${totalAmount >= 1.00 ? '¡Saldo suficiente para realizar una compra!' : 'Saldo insuficiente para realizar una compra.'}</p>
+      `;
+    }
+  }
+}
